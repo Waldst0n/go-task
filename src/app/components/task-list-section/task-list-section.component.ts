4ea47df67dfbc30b6896694791f99034d8c11ca1
Waldst0n <waldston.ws@gmail.com>
2025-12-13 11:33:45 -0300

    feat: New functions and updates were created within the tasks component and service.

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -1,19 +1,78 @@
 import { Component, inject } from '@angular/core';
 import { TaskCardComponent } from '../task-card/task-card.component';
 import { TaskService } from '../../services/task.service';
-
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { ITask } from '../../interfaces/task.interface';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { TaskStatus } from '../../type/task-status';
+import { TaskStatusEnum } from '../../enums/task-status.enums';
 @Component({
   selector: 'app-task-list-section',
-  imports: [TaskCardComponent],
+  imports: [TaskCardComponent, CdkDropList, CdkDrag, AsyncPipe, JsonPipe],
   templateUrl: './task-list-section.component.html',
   styleUrl: './task-list-section.component.css',
 })
 export class TaskListSectionComponent {
-  private readonly _taskService = inject(TaskService);
+  readonly _taskService = inject(TaskService);
 
-  ngOnInit() {
-    this._taskService.todoTasks.subscribe((todoList) => {
-      console.log(todoList);
-    });
+  onCardDrop(event: CdkDragDrop<ITask[]>) {
+    this.moveCardToolumn(event);
+
+    const taskId = event.item.data.id;
+    const taskCurentStatus = event.item.data.status;
+    const droppedColumn = event.container.id;
+
+    this.updateTaskStatus(taskId, taskCurentStatus, droppedColumn);
+  }
+
+  private updateTaskStatus(
+    taskId: string,
+    taskCurentStatus: TaskStatus,
+    droppedColumn: string
+  ) {
+    let taskNextStatus: TaskStatus;
+
+    switch (droppedColumn) {
+      case 'to-do-column':
+        taskNextStatus = TaskStatusEnum.TODO;
+        break;
+      case 'doing-column':
+        taskNextStatus = TaskStatusEnum.DOING;
+        break;
+      case 'done-column':
+        taskNextStatus = TaskStatusEnum.DONE;
+        break;
+      default:
+        throw Error('Coluna não identificada');
+    }
+
+    this._taskService.updateTaskStatus(
+      taskId,
+      taskCurentStatus,
+      taskNextStatus
+    );
+  }
+
+  private moveCardToolumn(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 import { ITask } from '../interfaces/task.interface';
 import { ItaskFormControls } from '../interfaces/task-form-controls.interface';
 import { TaskStatusEnum } from '../enums/task-status.enums';
 import { generateUniqueIdWithTimestamp } from '../utils/generate-unique-id-with-timestamp';
+import { TaskStatus } from '../type/task-status';
 
 @Injectable({
   providedIn: 'root',
@@ -11,15 +12,21 @@ import { generateUniqueIdWithTimestamp } from '../utils/generate-unique-id-with-
 export class TaskService {
   // A fazer
   private todoTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly todoTasks = this.todoTasks$.asObservable();
+  readonly todoTasks = this.todoTasks$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
 
   // Fazendo
   private doingTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly doingTasks = this.doingTasks$.asObservable();
+  readonly doingTasks = this.doingTasks$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
 
   // feitas
   private doneTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly doneTasks = this.doneTasks$.asObservable();
+  readonly doneTasks = this.doneTasks$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
 
   addTask(taskInfo: any) {
     const newTask: ITask = {
@@ -31,5 +38,38 @@ export class TaskService {
     const currentList = this.todoTasks$.value;
 
     this.todoTasks$.next([...currentList, newTask]);
+  }
+
+  updateTaskStatus(
+    taskId: string,
+    taskCurentStatus: TaskStatus,
+    nextStatus: TaskStatus
+  ) {
+    const currentTaskList = this.getTaskListByStatus(taskCurentStatus);
+    const nextTaskList = this.getTaskListByStatus(nextStatus);
+    const currentTask = currentTaskList.value.find(
+      (task) => task.id === taskId
+    );
+
+    if (currentTask) {
+      currentTask.status = nextStatus;
+
+      const currentTaskListWithoutTask = currentTaskList.value.filter(
+        (task) => task.id !== taskId
+      );
+      currentTaskList.next([...currentTaskListWithoutTask]);
+
+      nextTaskList.next([...nextTaskList.value, { ...currentTask }]);
+    }
+  }
+
+  private getTaskListByStatus(taskStatus: TaskStatus) {
+    const taskListObj = {
+      [TaskStatusEnum.TODO]: this.todoTasks$,
+      [TaskStatusEnum.DOING]: this.doingTasks$,
+      [TaskStatusEnum.DONE]: this.doneTasks$,
+    };
+
+    return taskListObj[taskStatus];
   }
 }
